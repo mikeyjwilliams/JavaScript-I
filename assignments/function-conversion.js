6a451f94f5de8b32aec0d233432b6f837726010e
mikey <manicdesigner@gmail.com>
2019-04-16 22:45:22 -0400

    refactor(func-conv) .map arrow style rework from map function style to arrow syntax

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -29,8 +29,9 @@ const subtract = (param1, param2) => param1 - param2;
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
-//   return num * 3;
+//     return num * 3;
 // });
-// console.log(triple);
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
