1215f2d4c3f6514cd998a94ddc085f1c51215bb9
mikey <manicdesigner@gmail.com>
2019-04-18 10:34:39 -0400

    refactor(arrays) challenge 6 redone with a for loop

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -355,11 +355,16 @@ for (let i = 0; i < carYears.length; i++) {
 }
 // --> stretch did with filter
 //oldCars = carYears.filter(year => year > 2000);
-console.log(oldCars.length);
+//console.log(oldCars.length);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
+for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
+        BMWAndAudi.push(inventory[i]);
+    }
+}
 // stretch did with filter
 //BMWAndAudi = inventory.filter(model => model.car_make === 'BMW' || model.car_make === 'Audi');
-//console.log(JSON.stringify(BMWAndAudi));
+console.log(JSON.stringify(BMWAndAudi));
